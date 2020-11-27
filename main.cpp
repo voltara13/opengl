3154dfb9d20261fbf0c8efe1798929914b55be12
Владислав Симонов <simonov.2019@azure.nstu.ru>
2020-11-27 16:35:18 +0700

    lw6

--- main.cpp
+++ main.cpp
@@ -1,20 +1,56 @@
 #include <GL/glut.h>
 #include <stdlib.h>
 
-/*  Initialize material property, light source, lighting model,
- *  and depth buffer.
- */
-void init(void)
+//void init(void) //Белая сфера: источник света - цветной
+//{
+//    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+//    GLfloat mat_shininess[] = { 50.0 };
+//    GLfloat global_ambient[] = { 0.1, 0.1, 0.3, 1.0 };
+//    glClearColor(0.0, 0.0, 0.0, 0.0);
+//    glShadeModel(GL_SMOOTH);
+//
+//    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+//    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+//	
+//
+//	//glLightModelfv(GL_LIGHT_MODEL_AMBIENT, global_ambient); //Фоновое освещение
+//
+//    //GLfloat light_position[] = { 1.0, 1.0, 1.0, 1.0 }; //Точечное освещение
+//    //glLightfv(GL_LIGHT0, GL_POSITION, light_position); 
+//    //glLightfv(GL_LIGHT0, GL_AMBIENT, global_ambient);
+//
+//    //GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 }; //Удаленное освещение
+//    //glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+//    //glLightfv(GL_LIGHT0, GL_AMBIENT, global_ambient);
+//
+//    //GLfloat light_position[] = { -2.0, 2.0, 1.0, 1.0 }; //Прожекторное освещение
+//    //GLfloat light_spot_direction[] = { -1.0, -2.0, -1.0, 1.0 };
+//    //glLightfv(GL_LIGHT0, GL_POSITION, light_position); 
+//    //glLightfv(GL_LIGHT0, GL_AMBIENT, global_ambient);
+//    //glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, light_spot_direction);
+//    //glLightf(GL_LIGHT0, GL_SPOT_CUTOFF, 90);
+//
+//
+//    glEnable(GL_LIGHTING);
+//    glEnable(GL_LIGHT0);
+//    glEnable(GL_DEPTH_TEST);
+//    glEnable(GL_COLOR_MATERIAL);
+//}
+
+void init(void) //Источник - белый свет: свойство материала - хром
 {
-    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat mat_shininess[] = { 50.0 };
+    GLfloat ambient[] = { 0.25, 0.25, 0.25, 1.0 };
+    GLfloat diffuse[] = { 0.4, 0.4, 0.4, 1.0 };
+    GLfloat specular[] = { 0.774597, 0.774597, 0.774597, 1.0 };
     GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
 
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glShadeModel(GL_SMOOTH);
-
-    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+	
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
+    glMaterialf(GL_FRONT, GL_SHININESS, 76.8);
     glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 
     glEnable(GL_LIGHTING);
@@ -26,8 +62,8 @@ void init(void)
 void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3f(1.0, 0.0, 0.0);
-    glutSolidSphere(1.0, 20, 16);
+    glColor3f(1.0, 1.0, 1.0);
+    glutSolidSphere(1.0, 100, 16);
     glFlush();
 }
 
@@ -52,7 +88,6 @@ void keyboard(unsigned char key, int x, int y)
     {
     case 27:
         exit(0);
-        break;
     }
 }
 
