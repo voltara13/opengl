4b1fc0bdfaf0a956242c71e471b714e57e00f178
Владислав Симонов <simonov.2019@azure.nstu.ru>
2020-11-30 22:51:15 +0700

    lw6

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-#include <GL/glut.h>
-#include <stdlib.h>
-
 //void init(void) //Белая сфера: источник света - цветной
 //{
 //    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
@@ -37,71 +34,79 @@
 //    glEnable(GL_COLOR_MATERIAL);
 //}
 
+
+#include <GL/glut.h>
+#include <stdlib.h>
+
+/*  Initialize material property, light source, lighting model,
+ *  and depth buffer.
+ */
 void init(void) //Источник - белый свет: свойство материала - хром
 {
-    GLfloat ambient[] = { 0.25, 0.25, 0.25, 1.0 };
-    GLfloat diffuse[] = { 0.4, 0.4, 0.4, 1.0 };
-    GLfloat specular[] = { 0.774597, 0.774597, 0.774597, 1.0 };
-    GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+	GLfloat mat_ambient[] = {0.25, 0.25, 0.25, 1.0};
+	GLfloat mat_diffuse[] = {0.4, 0.4, 0.4, 1.0};
+	GLfloat mat_specular[] = {0.774597, 0.774597, 0.774597, 1.0};
+	GLfloat mat_shininess = 76.8;
+	GLfloat light_position[] = {1.0, 1.0, 1.0, 0.0};
 
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glShadeModel(GL_SMOOTH);
-	
-    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
-    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
-    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
-    glMaterialf(GL_FRONT, GL_SHININESS, 76.8);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glShadeModel(GL_SMOOTH);
 
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_COLOR_MATERIAL);
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	glMaterialf(GL_FRONT, GL_SHININESS, mat_shininess);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_COLOR_MATERIAL);
 }
 
 void display(void)
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3f(1.0, 1.0, 1.0);
-    glutSolidSphere(1.0, 100, 16);
-    glFlush();
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glutSolidSphere(1.0, 100, 16);
+	glFlush();
 }
 
 void reshape(int w, int h)
 {
-    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    if (w <= h)
-        glOrtho(-1.5, 1.5, -1.5 * (GLfloat)h / (GLfloat)w,
-            1.5 * (GLfloat)h / (GLfloat)w, -10.0, 10.0);
-    else
-        glOrtho(-1.5 * (GLfloat)w / (GLfloat)h,
-            1.5 * (GLfloat)w / (GLfloat)h, -1.5, 1.5, -10.0, 10.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+	glViewport(0, 0, static_cast<GLsizei>(w), static_cast<GLsizei>(h));
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (w <= h)
+		glOrtho(-1.5, 1.5, -1.5 * static_cast<GLfloat>(h) / static_cast<GLfloat>(w),
+		        1.5 * static_cast<GLfloat>(h) / static_cast<GLfloat>(w), -10.0, 10.0);
+	else
+		glOrtho(-1.5 * static_cast<GLfloat>(w) / static_cast<GLfloat>(h),
+		        1.5 * static_cast<GLfloat>(w) / static_cast<GLfloat>(h), -1.5, 1.5, -10.0, 10.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
 void keyboard(unsigned char key, int x, int y)
 {
-    switch (key)
-    {
-    case 27:
-        exit(0);
-    }
+	switch (key)
+	{
+	case 27:
+		exit(0);
+	}
 }
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow(argv[0]);
-    init();
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutKeyboardFunc(keyboard);
-    glutMainLoop();
-    return 0;
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow(argv[0]);
+	init();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyboard);
+	glutMainLoop();
+	return 0;
 }
